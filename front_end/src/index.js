2b585cf6c98ad09fafc427b9199f0d24b750d846
Regis <boudinot.regis@yahoo.com>
2017-05-24 13:09:29 -0600

    better naming:

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,7 +1,7 @@
 const pageData = document.querySelector('#app').dataset;
 
 switch (pageData.pageName) {
-  case 'index-app':
+  case 'home#index':
     System.import('./home');
     break;
   default:
