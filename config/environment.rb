de82370a54fa8437a99add0fec1f1d8f3fd1b3ca
Regis <boudinot.regis@yahoo.com>
2017-05-28 03:36:57 -0600

    make dynamic config

--- config/environment.rb
+++ config/environment.rb
@@ -15,14 +15,25 @@ Dir[APP_ROOT.join("controllers", "*.rb")]
 
 # configure Server settings
 module SinIo
+  json_config = File.read(APP_ROOT.join("config", "gpio_config.json"))
+  parsed_config = JSON.parse(json_config)
+
   ## global pin state
-  AppPins = {17 => false}
+  Directions = parsed_config["directions"]
+  AppPins = parsed_config["pins"]
   GpioPins = {}
 
   # if a RPI ENV VAR is given to be true this code will run
   if ENV["RPI"] == ["true"]
     AppPins.each do |pin, v|
-      GpioPins[pin] = PiPiper::Pin.new(pin: pin, direction: :out)
+      GpioPins[pin] = PiPiper::Pin.new(
+        pin: pin,
+        direction: Directions[pin].to_sym
+       )
+    end
+
+    AppPins.each do |pin, state|
+      state ? GpioPins[pin].on : GpioPins[pin].off
     end
   end
 
